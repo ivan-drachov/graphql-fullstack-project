b619be530bbb02399f14da0fc4595a2f814ed388
Ivan Drachov <iv20052@gmail.com>
2022-06-13 09:39:51 +0300

    Second commit (client part with schema updating)

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import Header from './components/Header';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
+
+import AddClientModal from './components/AddClientModal';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming){
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming){
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+})
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5003/graphql',
+  //cache: new InMemoryCache(),
+  cache
+})
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project/>}/>
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
+      
+    </>
+    
+  );
+}
+
+export default App;
